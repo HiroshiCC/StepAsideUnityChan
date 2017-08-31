c078bc9df9de14adfc2f920e7c203412197cc199
高須　寛 <h-takasu@mwa.biglobe.ne.jp>
2017-08-31 13:41:53 +0900

    課題提出版
    Unity-chanより後ろになったらすぐに画面から消えるようにして、アイテムが消えることを確認。
    その語、Unity-chanより、15後方（画面からはずれた頃）になったら消えるようにした。

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -2,30 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class CoinController : MonoBehaviour {
+public class CarController : MonoBehaviour {
 
     // Unity-chanのオブジェクト
     private GameObject UnityChanObj;
 
     // Use this for initialization
-	void Start () {
-
+    void Start () {
         // Unity-chanのオブジェクトを取得
         UnityChanObj = GameObject.Find("unitychan");
-
-        //回転を開始する角度を設定
-        this.transform.Rotate(0, Random.Range(0, 360), 0);
     }
 
     // Update is called once per frame
     void Update () {
-        //回転
-        this.transform.Rotate(0, 3, 0);
-
-        // Unity-chanより、15後方になったら消す
         if (this.transform.position.z < ( UnityChanObj.transform.position.z - 15 ) )
         {
+            // Unity-chanより、15後方になったら消す
             Destroy(this.gameObject);
         }
     }
